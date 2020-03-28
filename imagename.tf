b2aa5a6196bf221c6ed51aad05fcd366e3854882
unknown <suzieboo@gmail.com>
2020-03-28 20:00:10 +1300

    scale set2

--- imagename.tf
+++ imagename.tf
@@ -2,4 +2,7 @@
 #   name                = "packer-ubuntu18-weatherapp_1585357450"
 #   resource_group_name = data.azurerm_resource_group.image.name
 # }
-data "azurerm_image" "image" { name = "packer-ubuntu18-weatherapp_1585357450" resource_group_name = data.azurerm_resource_group.image.name}
+data "azurerm_image" "image" { 
+    name = "packer-ubuntu18-weatherapp_1585357450" 
+    resource_group_name = data.azurerm_resource_group.image.name
+}
