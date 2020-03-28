82a017ea26504b9fa0667ab6dc83aebf95bc212d
unknown <suzieboo@gmail.com>
2020-03-28 19:00:48 +1300

    just a var for resourcegroup

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rg" {
-        name = "terraformResourceGroup"
+        name = var.resource_group_name
         location = var.location
 }
--- vars.tf
+++ vars.tf
@@ -7,3 +7,9 @@ variable ARM_SUBSCRIPTION_ID {}
 variable ARM_CLIENT_ID {}
 variable ARM_CLIENT_SECRET {}
 variable ARM_TENANT_ID {}
+
+variable "resource_group_name" {
+  description = "Resources craeted by terraform"
+  default     = "terraformResourceGroup"
+}
+
