376cd1abf7dee1ac5a06da78958a091f2bf3d8ae
unknown <suzieboo@gmail.com>
2020-03-28 18:14:14 +1300

    vars

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rg" {
         name = "terraformResourceGroup"
-        location = "auseast"
+        location = var.location
 }
---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  type    = string
+  default = "auseast"
+}
+
+variable ARM_SUBSCRIPTION_ID {}
+variable ARM_CLIENT_ID {}
+variable ARM_CLIENT_SECRET {}
+variable ARM_TENANT_ID {}
