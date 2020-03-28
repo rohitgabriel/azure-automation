a098c52aa156b9c9de9f76c1f7367741988fdf78
unknown <suzieboo@gmail.com>
2020-03-28 19:12:48 +1300

    adding a vnet and subnet

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,4 +1,8 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "vmss" {
         name = var.resource_group_name
         location = var.location
+        tags {
+          environment = "demo"
+          costcenter = "007"
+        }
 }
---
+++ vnet.tf
@@ -0,0 +1,31 @@
+resource "azurerm_virtual_network" "vmss" {
+  name                = "vmss-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.vmss.name
+
+  tags {
+    environment = "demo"
+    costcenter = "007"
+  }
+}
+
+resource "azurerm_subnet" "vmss" {
+  name                 = "vmss-subnet"
+  resource_group_name  = azurerm_resource_group.vmss.name
+  virtual_network_name = azurerm_virtual_network.vmss.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_public_ip" "vmss" {
+  name                         = "vmss-public-ip"
+  location                     = var.location
+  resource_group_name          = azurerm_resource_group.vmss.name
+  allocation_method            = "static"
+  domain_name_label            = azurerm_resource_group.vmss.name
+
+  tags {
+    environment = "demo"
+    costcenter = "007"
+  }
+}
