d9efd658421996acde3f8b058f4070f5be0eca6d
unknown <suzieboo@gmail.com>
2020-03-28 19:17:11 +1300

    adding a vnet and subnet2

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "vmss" {
-        name = var.resource_group_name
-        location = var.location
-        tags {
-          environment = "demo"
-          costcenter = "007"
-        }
+  name = var.resource_group_name
+  location = var.location
+  tags {
+    environment = "demo"
+    costcenter = "007"
+  }
 }
