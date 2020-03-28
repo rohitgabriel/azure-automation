283593b078adaeb606a155cfac3f6ff38a629303
unknown <suzieboo@gmail.com>
2020-03-28 23:26:00 +1300

    terraform apply

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,12 +3,12 @@ pipeline {
     environment {
         AZURE_CRED=credentials('AZURE_CRED')
         AZURE_ID=credentials('AZURE_ID')
-        AZUREPUB=credentials('AZUREPUB')
-        client_id="${AZURE_CRED_USR}"
-        client_secret="${AZURE_CRED_PSW}"
-        tenant_id="${AZURE_ID_USR}"
-        subscription_id="${AZURE_ID_PSW}"
-        location="australiaeast"
+        // AZUREPUB=credentials('AZUREPUB')
+        // client_id="${AZURE_CRED_USR}"
+        // client_secret="${AZURE_CRED_PSW}"
+        // tenant_id="${AZURE_ID_USR}"
+        // subscription_id="${AZURE_ID_PSW}"
+        // location="australiaeast"
         
     }
     stages {
@@ -62,11 +62,20 @@ pipeline {
         }
         stage("terraform apply") {
             steps {
+                withCredentials([azureServicePrincipal(credentialsId: 'AZURE_ACCOUNT_CREDS',
+                                    subscriptionIdVariable: 'ARM_SUBSCRIPTION_ID',
+                                    clientIdVariable: 'ARM_CLIENT_ID',
+                                    clientSecretVariable: 'ARM_CLIENT_SECRET',
+                                    tenantIdVariable: 'ARM_TENANT_ID')]) {
                 sh '''
-                /usr/local/bin/terraform apply -input=false tfplan
+
+                export TF_VAR_sshkey="${AZUREPUB_PSW}"
+                /usr/local/bin/terraform apply -input=false tfplan -var 'ARM_SUBSCRIPTION_ID="${ARM_SUBSCRIPTION_ID}"' -var 'ARM_CLIENT_ID="${ARM_CLIENT_ID}"' -var 'ARM_CLIENT_SECRET="${ARM_CLIENT_SECRET}"' -var 'ARM_TENANT_ID="${ARM_TENANT_ID}"'
                 '''
+                }
             }
         }
+      
         // stage("Refresh Instance in ASG") {
         //     steps {
         //         withAWS(credentials: 'TerraformAWSCreds', region: 'ap-southeast-2') {
