b706263c98fbd72c12c9f5beb0dd643284bae222
unknown <suzieboo@gmail.com>
2020-03-28 23:47:56 +1300

    packer in jenkins

--- build-image-with-packer.sh
+++ build-image-with-packer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ARTIFACT=`packer build -machine-readable packer/azure-ubuntu-nodejs.json | grep 'Image Name'`
 AMI_ID=`echo $ARTIFACT | awk -F "'" '{print $2}'`
-
-echo 'data "azurerm_image" "image" {
-  name                = "'${AMI_ID}'"
-  resource_group_name = data.azurerm_resource_group.image.name
-}' > imagename.tf
+echo $AMI_ID
+# echo 'data "azurerm_image" "image" {
+#   name                = "'${AMI_ID}'"
+#   resource_group_name = data.azurerm_resource_group.image.name
+# }' > imagename.tf
