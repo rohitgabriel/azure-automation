4a4eba48a1e5d2c2df404727757bd6dcf1217319
unknown <suzieboo@gmail.com>
2020-03-29 13:50:25 +1300

    install beats

--- shell/install_software.sh
+++ shell/install_software.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-sudo apt-get update
+cd /tmp
+#auditbeat
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt-get update && sudo apt-get install auditbeat
+sudo systemctl enable auditbeat
+#metricbeat
+curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.6.1-amd64.deb
+sudo dpkg -i metricbeat-7.6.1-amd64.deb
+
